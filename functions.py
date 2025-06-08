a2c2a74c45ab79a71c927784ecdc5b4b068d1aab
Katelyn Hoang <khoang3@uw.edu>
2025-06-08 16:44:33 -0700

    OAuth and token

--- functions.py
+++ functions.py
@@ -1,6 +1,22 @@
 # Consider using Spotipy, get track audio features!!
 from authlib.integrations.requests_client import OAuth2Session
 from keys import CLIENT_ID, CLIENT_SECRET
+import pprint
+
+scope = "user-top-read"
+
+client = OAuth2Session(CLIENT_ID, CLIENT_SECRET, scope=scope, redirect_uri="https://katelynhoang.pythonanywhere.com/")
+authorization_endpoint = "https://accounts.spotify.com/authorize"
+uri, state = client.create_authorization_url(authorization_endpoint)
+print("Please go to this URL and follow the prompts: {}".format(uri))
+
+authorization_response = input("Once you are redirected by your browser, copy the URL from your browser's address bar and enter it here: ")
+
+token_endpoint = "https://accounts.spotify.com/api/token"
+token = client.fetch_token(token_endpoint, authorization_response=authorization_response)
+api_endpoint = "https://api.spotify.com/v1"
+resp = client.get(api_endpoint + "/me/top/artists?time_range=long_term&limit=5")
+pprint.pprint(resp.text)
 
 
 
