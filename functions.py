c1cf83282f3e31e2e4a1b81cc22347e2541a2443
josueasalg <josuaye@uw.edu>
2025-06-09 17:19:51 -0700

    added get books

--- functions.py
+++ functions.py
@@ -58,3 +58,30 @@ def get_cover(olid):
     if olid is None:
         return None
     return "https://covers.openlibrary.org/b/olid/" + olid + "-M.jpg"
+
+def get_books(lst_keywords):
+   books = {}
+   index = 0
+   for i in range(10):
+       if index < len(lst_keywords):
+           genre = lst_keywords[index]
+       else:
+           index = 0
+           genre = lst_keywords[index]
+       APIcall = "http://openlibrary.org/search.json?q=" + genre
+       with urllib.request.urlopen(APIcall) as response:
+           resp = response.read()
+           resp_str = resp.decode("utf-8")
+       data = json.loads(resp_str)
+       works = data["docs"]
+       result = works[i]
+       title = result["title"]
+       author = result["author_name"][0]
+       if len(result["cover_edition_key"]) != 0:
+           cover = result["cover_edition_key"]
+       else:
+           cover = None
+       if title not in books.keys():
+           books[title] = {"author": author, "cover": cover}
+       index += 1
+   return books
